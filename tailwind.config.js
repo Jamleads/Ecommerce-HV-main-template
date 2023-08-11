aeda3854e0d5e457e90f078082279aabfc132842
AdetunjiJakes <68664742+Jamleads@users.noreply.github.com>
2023-08-11 16:01:00 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        pink: "#FB2E86",
+        reddy: "#FB2448",
+        blue: "#2F1AC4",
+        navyBlue: "#151875",
+        offNavyBlue: "#3F509E",
+        skyBlue: "#F2F0FF",
+        offSkyBlue: "#F6F7FB",
+        purpple: "#7E33E0",
+        mainWhite: "#FFF",
+        offWhite: "#F1F1F1",
+        offGreen: "#19D16F",
+      },
+    },
+  },
+  plugins: [],
+};
